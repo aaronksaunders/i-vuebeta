5a1c98d40e94f4151667c7db8a13f9589de69956
Aaron Saunders <aaronksaunders@gmail.com>
2020-09-04 09:43:35 -0400

    formatting

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -4,12 +4,12 @@
       <ion-tabs>
         <ion-tab-bar slot="bottom">
           <ion-tab-button tab="tab1" href="/tabs/tab1">
-            <ion-icon :icon="triangle" />
+            <ion-icon :icon="homeOutline" />
             <ion-label>Tab 1</ion-label>
           </ion-tab-button>
             
           <ion-tab-button tab="tab2" href="/tabs/tab2">
-            <ion-icon :icon="ellipse" />
+            <ion-icon :icon="settingsOutline" />
             <ion-label>Tab 2</ion-label>
           </ion-tab-button>
           
@@ -25,16 +25,16 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonContent, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {  square, settingsOutline, homeOutline } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonContent, IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
+      homeOutline, 
       square, 
-      triangle,
+      settingsOutline,
     }
   }
 }
